ad2b22a96cdfe63953a0017e2ab41a2278fb9afe
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-10-30 13:14:33 -0600

    Add day/night theme spinner in SettingsActivity along with a very basic implementation of its behavior.

---
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/utils/Constants.java
@@ -0,0 +1,8 @@
+package cy.agorise.bitsybitshareswallet.utils;
+
+public class Constants {
+    /**
+     * Key used to store the night mode setting into the shared preferences
+     */
+    public static final String KEY_NIGHT_MODE_ACTIVATED = "key_night_mode_activated";
+}
