f4759abc7237c4e20ac77b6d6aa9bd7f433a4e8c
dtvv <coritocorito@hotmail.com>
2018-10-31 15:22:44 -0600

    Convert Constants.java to kotlin

--- app/src/main/java/cy/agorise/bitsybitshareswallet/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package cy.agorise.bitsybitshareswallet.utils;
-
-public class Constants {
-    /**
-     * Key used to store the night mode setting into the shared preferences
-     */
-    public static final String KEY_NIGHT_MODE_ACTIVATED = "key_night_mode_activated";
-}
