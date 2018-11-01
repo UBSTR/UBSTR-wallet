da9e0225cc59166b90e9e7bf6d181c1a542d2643
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-10-31 18:51:41 -0600

    Create Operation, Transfer and Equivalent value Entities for the Room database.

---
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/models/EquivalentValue.kt
@@ -0,0 +1,29 @@
+package cy.agorise.bitsybitshareswallet.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "equivalent_values",foreignKeys =
+    [ForeignKey(
+        entity = Transfer::class,
+        parentColumns = ["operation_id"],
+        childColumns = ["transfer_id"],
+        onUpdate = ForeignKey.CASCADE,
+        onDelete = ForeignKey.CASCADE
+    ), ForeignKey(
+        entity = Asset::class,
+        parentColumns = ["id"],
+        childColumns = ["asset_id"],
+        onUpdate = ForeignKey.CASCADE,
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class EquivalentValue (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") val id: Long,
+    @ColumnInfo(name = "transfer_id") val transferId: String,
+    @ColumnInfo(name = "value") val value: Long,
+    @ColumnInfo(name = "asset_id") val assetId: String
+)
