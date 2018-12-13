82efa60a367de49dcdfbb8e805146fed4a6f5437
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-12-12 20:56:20 -0600

    Introduce the new Android Architecture Components' Navigation. - Remove the old MainActivity layout that included a BottomNavigationView. - Start using the Navigation AAC and make the BalancesFragment the default Fragment so that it is the first to appear.

--- app/src/main/java/cy/agorise/bitsybitshareswallet/activities/MainActivity.kt
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/activities/MainActivity.kt
@@ -1,74 +1,16 @@
 package cy.agorise.bitsybitshareswallet.activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import cy.agorise.bitsybitshareswallet.R
-import cy.agorise.bitsybitshareswallet.fragments.BalancesFragment
-import cy.agorise.bitsybitshareswallet.fragments.MerchantsFragment
-import cy.agorise.bitsybitshareswallet.fragments.ReceiveTransactionFragment
-import cy.agorise.bitsybitshareswallet.fragments.SendTransactionFragment
 import cy.agorise.graphenej.api.ConnectionStatusUpdate
 import cy.agorise.graphenej.models.JsonRpcResponse
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : ConnectedActivity() {
     private val TAG = this.javaClass.simpleName
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_receive -> {
-                loadFragment(ReceiveTransactionFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_balances -> {
-                loadFragment(BalancesFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_merchants -> {
-                loadFragment(MerchantsFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_send -> {
-                loadFragment(SendTransactionFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        navigation.selectedItemId = R.id.navigation_balances
-    }
-
-    private fun loadFragment(fragment: Fragment) {
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.fragment_frame, fragment)
-        ft.commit()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflates the menu and places it in the toolbar
-        menuInflater.inflate(R.menu.menu_main, menu)
-
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return if (item!!.itemId == R.id.menu_settings) {
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-            true
-        } else {
-            super.onOptionsItemSelected(item)
-        }
     }
 
     override fun handleJsonRpcResponse(response: JsonRpcResponse<*>) {
@@ -80,11 +22,6 @@ class MainActivity : ConnectedActivity() {
      * @param connectionStatusUpdate  Connection status update.
      */
     override fun handleConnectionStatusUpdate(connectionStatusUpdate: ConnectionStatusUpdate) {
-        when (connectionStatusUpdate.updateCode) {
-            ConnectionStatusUpdate.CONNECTED -> { /* Do nothing for now */ }
-            ConnectionStatusUpdate.DISCONNECTED -> { /* Do nothing for now */ }
-            ConnectionStatusUpdate.AUTHENTICATED -> {}//updateBalances() }
-            ConnectionStatusUpdate.API_UPDATE -> { }
-        }
+
     }
 }
