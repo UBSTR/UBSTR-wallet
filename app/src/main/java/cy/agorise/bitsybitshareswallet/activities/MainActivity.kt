3d6ce645b5ba5cd7a1af1b4547da3067a09dcc30
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2018-12-12 21:30:27 -0600

    Make proper configurations to enable the Navigation component and make use of it to add the navigation to the Merchants fragment and back to the Balances fragment.

--- app/src/main/java/cy/agorise/bitsybitshareswallet/activities/MainActivity.kt
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/activities/MainActivity.kt
@@ -1,16 +1,61 @@
 package cy.agorise.bitsybitshareswallet.activities
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import cy.agorise.bitsybitshareswallet.R
 import cy.agorise.graphenej.api.ConnectionStatusUpdate
 import cy.agorise.graphenej.models.JsonRpcResponse
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : ConnectedActivity() {
     private val TAG = this.javaClass.simpleName
 
+    private lateinit var appBarConfiguration : AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment? ?: return
+
+        // Set up Action Bar
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                Integer.toString(destination.id)
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Have the NavigationUI look for an action or destination matching the menu
+        // item id and navigate there if found.
+        // Otherwise, bubble up to the parent.
+        return item.onNavDestinationSelected(findNavController(R.id.navHostFragment))
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // Allows NavigationUI to support proper up navigation or the drawer layout
+        // drawer menu, depending on the situation
+        return findNavController(R.id.navHostFragment).navigateUp(appBarConfiguration)
     }
 
     override fun handleJsonRpcResponse(response: JsonRpcResponse<*>) {
--- app/src/main/java/cy/agorise/bitsybitshareswallet/fragments/BalancesFragment.kt
+++ app/src/main/java/cy/agorise/bitsybitshareswallet/fragments/BalancesFragment.kt
@@ -3,10 +3,8 @@ package cy.agorise.bitsybitshareswallet.fragments
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,6 +31,8 @@ class BalancesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_balances, container, false)
     }
 
@@ -70,5 +70,9 @@ class BalancesFragment : Fragment() {
         mTransferDetailViewModel.getAll(userId).observe(this, Observer<List<TransferDetail>> { transfersDetails ->
             transfersDetailsAdapter.replaceAll(transfersDetails)
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_balances, menu)
     }
 }
